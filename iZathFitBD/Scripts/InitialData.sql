303fd1c983c4ae4d9db2871be7ca065181474270
David Legendre <legendre2013@hotmail.com>
2023-10-06 17:42:27 -0500

    cambio

--- iZathFitBD/Scripts/InitialData.sql
+++ iZathFitBD/Scripts/InitialData.sql
@@ -25,11 +25,11 @@ GO
 	insert into dbo.Persona(IdPersona, Nombres, 
 	Apellidos,Direccion,Email,Fech_Nacimiento,Identificacion,Telefono,idRol, 
 	idGenero, idtipoIdentificacion, idOcupacion, NumeroEmergencia1) values
-	(@uid,'David', 'Legendre', 'Urb. La Perla','dlegendre74@gmail.com','1996-09-06',
-	'49001564','914847720',1,1,1,5, '914847720')
+	(@uid,'nombres', 'apellidos', 'direccion','email','año-mes-dia',
+	'identificacion','telefono',1,1,1,5, 'numemergencia')
 	declare @uidusuario uniqueidentifier
 	set @uidusuario = newid()
 	insert into dbo.Usuario(idUsuario,IdPersona, usuario, contrasena,IsActivo) values
-	(@uidusuario,@uid, 'David', 'David123', 1)
+	(@uidusuario,@uid, 'usuario', 'password', 1)
 
 GO
